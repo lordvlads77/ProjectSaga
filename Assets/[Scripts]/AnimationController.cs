6eeab823ec8f9d9d33883995a53f28df2f8b84c2
Eva Luna Álvarez <zap363@amerike.edu.mx>
2022-09-14 19:11:52 -0500

    Added Animation Controller

---
+++ Assets/[Scripts]/AnimationController.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ProjectSaga
+{
+    public class AnimationController : MonoBehaviour
+    {
+        public Animator _Animator = default;
+        private int _vertical;
+        private int _horizontal;
+
+        [SerializeField] private bool _rotationAllowed = default;
+
+        public void Awake()
+        {
+            _Animator = GetComponent<Animator>();
+            _vertical = Animator.StringToHash("Vertical");
+            _horizontal = Animator.StringToHash("Horizontal");
+        }
+
+        public void refreshAnimValues(float _verticalMovement, float _horizontalMovement)
+        {
+            #region Vertical
+                float _vert = 0;
+
+                if (_verticalMovement > 0 && _verticalMovement < 0.55f)
+                {
+                    _vert = 0.5f;
+                }
+                else if (_verticalMovement > 0.55f)
+                {
+                    _vert = 1;
+                }
+                else if (_verticalMovement < 0 && _verticalMovement > -0.55f)
+                {
+                    _vert = -0.5f;
+                }
+                else if (_verticalMovement < -0.55f)
+                {
+                    _vert = -1;
+                }
+                else
+                {
+                    _vert = 0;
+                }
+
+            #endregion
+
+            #region Horizontal
+
+            float _hori = 0;
+
+            if (_horizontalMovement > 0 && _horizontalMovement < 0.55f)
+            {
+                _hori = 0.5f;
+            }
+            else if (_horizontalMovement > 0.55f)
+            {
+                _hori = 1;
+            }
+            else if (_horizontalMovement < 0 && _horizontalMovement > -0.55f)
+            {
+                _hori = -0.5f;
+            }
+            else if (_horizontalMovement < -0.55f)
+            {
+                _hori = -1;
+            }
+            else
+            {
+                _hori = 0;
+            }
+
+            #endregion
+        }
+    }
+}
