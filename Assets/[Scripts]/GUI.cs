48163d789cb5689afc6292c076609b871b2738c1
Eva Luna Alvarez Calderon <zap363@amerike.edu.mx>
2022-09-28 14:58:49 -0500

    UI Started Missing Death

---
+++ Assets/[Scripts]/GUI.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ProjectSaga
+{
+    public class GUI : MonoBehaviour
+    {
+        public static GUI Instance { get; private set; }
+        [Header("GUI Screens")]
+        [SerializeField] private GameObject _playScreen = default;
+        [SerializeField] private GameObject _pauseScreen = default;
+        [SerializeField] private GameObject _saveScreen = default;
+        [SerializeField] private GameObject _deathScreen = default;
+        [SerializeField] private GameObject _ingamePscreen = default;
+        [SerializeField] private GameObject _saved = default;
+        [SerializeField] private GameObject _loaded = default;
+
+        private void Awake()
+        {
+            Instance = this;
+            if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void Start()
+        {
+            Time.timeScale = 0;
+        }
+
+        public void playButton()
+        {
+            _playScreen.SetActive(false);
+            _ingamePscreen.SetActive(true);
+            Time.timeScale = 1;
+        }
+
+        public void pause()
+        {
+            _ingamePscreen.SetActive(false);
+            _pauseScreen.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        public void deathScreen()
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        public void Notpause()
+        {
+            _pauseScreen.SetActive(false);
+            _ingamePscreen.SetActive(true);
+            Time.timeScale = 1;
+        }
+
+        public void SaveGame()
+        {
+            _pauseScreen.SetActive(false);
+            _saveScreen.SetActive(true);
+        }
+
+        public void Saved()
+        {
+            _saveScreen.SetActive(false);
+            _saved.SetActive(true);
+            
+        }
+
+        public void Loaded()
+        {
+            _saveScreen.SetActive(false);
+            _loaded.SetActive(true);
+        }
+    }   
+}
