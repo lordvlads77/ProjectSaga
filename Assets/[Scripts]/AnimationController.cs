696f03255a19ea3a081ca6180f2f946aa352e749
lordvlads77 <zap363@amerike.edu.mx>
2022-10-10 13:47:23 -0500

    Level Design Packs y Cleaning

--- Assets/[Scripts]/AnimationController.cs
+++ Assets/[Scripts]/AnimationController.cs
@@ -7,82 +7,26 @@ namespace ProjectSaga
 {
     public class AnimationController : MonoBehaviour
     {
-        public Animator _Animator = default;
-        private int _vertical;
-        private int _horizontal;
-        private int _ismoving;
-
-        [SerializeField] private bool _rotationAllowed = default;
+        public static AnimationController Instance { get; private set; }
+        public Animator _animator = default;
+        private readonly int _isMov = Animator.StringToHash("isMoving");
 
         public void Awake()
         {
-            _Animator = GetComponent<Animator>();
-            _vertical = Animator.StringToHash("Vertical");
-            _horizontal = Animator.StringToHash("Horizontal");
-            _ismoving = Animator.StringToHash("isMoving");
+            
         }
 
-        public void refreshAnimValues(float _verticalMovement, float _horizontalMovement)
+        public void Moving()
         {
-            #region Vertical
-                float _vert = 0;
-
-                if (_verticalMovement > 0 && _verticalMovement < 0.55f)
-                {
-                    _vert = 0.5f;
-                }
-                else if (_verticalMovement > 0.55f)
-                {
-                    _vert = 1;
-                }
-                else if (_verticalMovement < 0 && _verticalMovement > -0.55f)
-                {
-                    _vert = -0.5f;
-                }
-                else if (_verticalMovement < -0.55f)
-                {
-                    _vert = -1;
-                }
-                else
-                {
-                    _vert = 0;
-                }
-
-            #endregion
-
-            #region Horizontal
-
-            float _hori = 0;
-
-            if (_horizontalMovement > 0 && _horizontalMovement < 0.55f)
-            {
-                _hori = 0.5f;
-            }
-            else if (_horizontalMovement > 0.55f)
-            {
-                _hori = 1;
-            }
-            else if (_horizontalMovement < 0 && _horizontalMovement > -0.55f)
-            {
-                _hori = -0.5f;
-            }
-            else if (_horizontalMovement < -0.55f)
-            {
-                _hori = -1;
-            }
-            else
-            {
-                _hori = 0;
-            }
-
-            #endregion
-            
-            #region Idle
-            
-                
-            
-            #endregion
+            _animator.SetBool("isMoving", true);
         }
+
+        public void notMoving()
+        {
+            _animator.SetBool("isMoving", false);
+        }
+
+       
 
        
     }
--- Assets/[Scripts]/Player/MobileController.cs
+++ Assets/[Scripts]/Player/MobileController.cs
@@ -3,69 +3,70 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MobileController : MonoBehaviour
+namespace ProjectSaga
 {
-    [SerializeField] private PlayerActions _playerActions = default;
-    private CharacterController controller;
-    // player velocity handles the ability to jump by setting it to 0
-    private Vector3 playerVelocity;
-    private bool groundedPlayer;
-    [SerializeField] private float playerSpeed = 2.0f;
-    [SerializeField] private float jumpHeight = 1.0f;
-    [SerializeField] private float gravityValue = -9.81f;
-    [Header("Animation Stuff")]
-    public Animator anim = default;
-    private readonly int _moving = Animator.StringToHash("isMoving");
-
-    private void Awake()
+    public class MobileController : MonoBehaviour
     {
-        _playerActions = new PlayerActions();
-        controller = GetComponent<CharacterController>();
-    }
-
-    private void OnEnable()
-    {
-        _playerActions.Enable();
-    }
-
-    private void OnDisable()
-    {
-        _playerActions.Disable();
-    }
-
-    void Update()
-    {
-        /*this makes sure the player y axis is 0, and also tells the game the player is grounded
-         if the axis is less than 0*/
-        groundedPlayer = controller.isGrounded;
-        if (groundedPlayer && playerVelocity.y < 0)
+        [SerializeField] private PlayerActions _playerActions = default;
+        private CharacterController controller;
+        // player velocity handles the ability to jump by setting it to 0
+        private Vector3 playerVelocity;
+        private bool groundedPlayer;
+        [SerializeField] private float playerSpeed = 2.0f;
+        [SerializeField] private float jumpHeight = 1.0f;
+        [SerializeField] private float gravityValue = -9.81f;
+    
+        private void Awake()
         {
-            playerVelocity.y = 0f;
+            _playerActions = new PlayerActions();
+            controller = GetComponent<CharacterController>();
         }
-
-        Vector2 _inputMovement = _playerActions.PlayerMoves.Move.ReadValue<Vector2>();
-        Vector3 move = new Vector3(_inputMovement.y, 0f, _inputMovement.x);
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
+    
+        private void OnEnable()
         {
-            gameObject.transform.forward = move; ;
-            anim.SetBool("isMoving", true);
+            _playerActions.Enable();
         }
-
-        if (move == Vector3.zero)
+    
+        private void OnDisable()
         {
-            anim.SetBool("isMoving", false);
+            _playerActions.Disable();
         }
-
-        // Changes the height position of the player..
-        //This enables the player to jump
-        if (_playerActions.PlayerMoves.Jump.triggered && groundedPlayer)
+    
+        void Update()
         {
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            /*this makes sure the player y axis is 0, and also tells the game the player is grounded
+             if the axis is less than 0*/
+            groundedPlayer = controller.isGrounded;
+            if (groundedPlayer && playerVelocity.y < 0)
+            {
+                playerVelocity.y = 0f;
+            }
+    
+            Vector2 _inputMovement = _playerActions.PlayerMoves.Move.ReadValue<Vector2>();
+            Vector3 move = new Vector3(_inputMovement.y, 0f, _inputMovement.x);
+            controller.Move(move * Time.deltaTime * playerSpeed);
+    
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+                AnimationController.Instance.Moving();
+            }
+    
+            if (move == Vector3.zero)
+            {
+                AnimationController.Instance.notMoving();
+            }
+    
+            // Changes the height position of the player..
+            //This enables the player to jump
+            if (_playerActions.PlayerMoves.Jump.triggered && groundedPlayer)
+            {
+                playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            }
+    
+            playerVelocity.y += gravityValue * Time.deltaTime;
+            controller.Move(playerVelocity * Time.deltaTime);
         }
-
-        playerVelocity.y += gravityValue * Time.deltaTime;
-        controller.Move(playerVelocity * Time.deltaTime);
-    }
+    }    
 }
+
