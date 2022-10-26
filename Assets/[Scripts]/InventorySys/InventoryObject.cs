dc5cd20cc9f6448d3085dfb8e9e516ac5feade0f
lordvlads77 <zap363@amerike.edu.mx>
2022-10-26 14:14:58 -0500

    Continue Working on the save and load function

--- Assets/[Scripts]/InventorySys/InventoryObject.cs
+++ Assets/[Scripts]/InventorySys/InventoryObject.cs
@@ -1,29 +1,54 @@
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
 namespace ProjectSaga
 {
     [CreateAssetMenu (fileName = "New Inventory", menuName = "Inventory System/Inventory")]
-    public class InventoryObject : ScriptableObject
+    public class InventoryObject : ScriptableObject, ISerializationCallbackReceiver
     {
+        public string _savePath = default;
+        public ItemDataBaseObject databaseObject;
         public List<InventorySlot> _Container = new List<InventorySlot>();
 
         public void AddItem(ItemObject _item, int _amount)
         {
-            bool _hasItem = false;
             for (int i = 0; i < _Container.Count; i++)
             {
                 if (_Container[i]._itemObject == _item)
                 {
                     _Container[i].AddAmount(_amount);
-                    _hasItem = true;
-                    break;
+                    return;
                 }
             }
+            _Container.Add(new InventorySlot(databaseObject.GetId[_item], _item, _amount));   
+            
+        }
 
-            if (!_hasItem)
+        public void Save()
+        {
+            string _saveData = JsonUtility.ToJson(this, true);
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            FileStream fileStream = File.Create(string.Concat(Application.persistentDataPath, _savePath));
+            binaryFormatter.Serialize(fileStream, _saveData);
+            fileStream.Close();
+        }
+
+        public void Load()
+        {
+            //Write the load function to be explained on 14:45
+        }
+
+        public void OnBeforeSerialize()
+        {
+        }
+
+        public void OnAfterDeserialize()
+        {
+            for (int i = 0; i < _Container.Count; i++)
             {
-                _Container.Add(new InventorySlot(_item, _amount));   
+                _Container[i]._itemObject = databaseObject._getItem[_Container[i]._ID_];
             }
         }
     }
@@ -31,11 +56,13 @@ namespace ProjectSaga
     [System.Serializable]
     public class InventorySlot
     {
+        public int _ID_ = default;
         [SerializeField] public ItemObject _itemObject;
         [SerializeField] public int _amount_ = default;
 
-        public InventorySlot(ItemObject _item, int _amount)
+        public InventorySlot(int _id, ItemObject _item, int _amount)
         {
+            _ID_ = _id;
             _itemObject = _item;
             _amount_ = _amount;
         }
--- Assets/[Scripts]/InventorySys/ItemDataBaseObject.cs
+++ Assets/[Scripts]/InventorySys/ItemDataBaseObject.cs
@@ -3,18 +3,25 @@ using UnityEngine;
 
 namespace ProjectSaga
 {
+    [CreateAssetMenu(fileName = "New Item Database", menuName = "Inventory System/Items/Database")]
     public class ItemDataBaseObject : ScriptableObject, ISerializationCallbackReceiver
     {
-        public ItemObject[] Items;
+        [SerializeField] public ItemObject[] Items;
         public Dictionary<ItemObject, int> GetId = new Dictionary<ItemObject, int>();
+        public Dictionary<int, ItemObject> _getItem = new Dictionary<int, ItemObject>();
         public void OnBeforeSerialize()
         {
-            throw new System.NotImplementedException();
         }
 
         public void OnAfterDeserialize()
         {
-            throw new System.NotImplementedException();
+            GetId = new Dictionary<ItemObject, int>();
+            _getItem = new Dictionary<int, ItemObject>();
+            for (int i = 0; i < Items.Length; i++)
+            {
+                GetId.Add(Items[i], i);
+                _getItem.Add(i, Items[i]);
+            }
         }
     }    
 }
